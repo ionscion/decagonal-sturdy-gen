fa2a4a86600982c8a8f08f9c72fe29e458ffada4
bensboto <bensboto@gmail.com>
2023-02-26 15:49:06 -0700

    added conditional questions to inquirer prompts based on role

--- index.js
+++ index.js
@@ -34,6 +34,24 @@ function initInquirer() {
         message: "What is team member's role at the company?",
         choices: ["Employee", "Manager", "Engineer", "Intern"],
       },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        when: (answers) => answers.role === "Manager",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub username?",
+        when: (answers) => answers.role === "Engineer",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school name?",
+        when: (answers) => answers.role === "Intern",
+      },
     ])
     .then((answers) => {
       const { name, id, email, role } = answers;
