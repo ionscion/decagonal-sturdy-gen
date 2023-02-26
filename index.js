c9a8f1186f20fe5b418b2adb19af2a82e0774b30
bensboto <bensboto@gmail.com>
2023-02-26 12:26:45 -0700

    pseudocode and installed jest, added basic test to make sure it connected

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Build out classes
+ * Need to build out inquirer prompts
+ * similar to last homework, collect answers for each class invoked
+ * Compile all the answers to template literal for the html
+ * CSS stylesheet, may want to build this out ahead of time
+ * write HTML to file, may want to use append in case more classes are added later
+ */
